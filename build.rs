1fc36d99b91262b7f747080fc77e58e9644c5651
Nathan Kent <nkent2@cs.rochester.edu>
2019-02-04 16:34:24 -0500

    Clean up feature dependencies and exclusivity

--- build.rs
+++ build.rs
@@ -2,25 +2,29 @@ use cmake::Config;
 use std::{env, path::PathBuf};
 
 fn main() {
-    if cfg!(feature = "build-nng") {
-        let dst = cmake();
-        // Check output of `cargo build --verbose`, should see something like:
-        // -L native=/path/runng/target/debug/build/runng-sys-abc1234/out
-        // That contains output from cmake
-        println!(
-            "cargo:rustc-link-search=native={}",
-            dst.join("lib").display()
-        );
-        println!(
-            "cargo:rustc-link-search=native={}",
-            dst.join("lib64").display()
-        );
+    #[cfg(feature = "build-nng")]
+    {
+        let generator = generator();
+        let stats = if cfg!(feature = "nng-stats") { "ON" } else { "OFF" };
+        let tls = if cfg!(feature = "nng-tls") { "ON" } else { "OFF" };
 
-        // Tell rustc to use nng static library
+        let dst = Config::new("nng")
+            .generator(generator)
+            .define("NNG_TESTS", "OFF")
+            .define("NNG_TOOLS", "OFF")
+            .define("NNG_ENABLE_STATS", stats)
+            .define("NNG_ENABLE_TLS", tls)
+            .build();
+
+        println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
+        println!("cargo:rustc-link-search=native={}", dst.join("lib64").display());
         println!("cargo:rustc-link-lib=static=nng");
-    } else {
+    }
+    #[cfg(not(feature = "build-nng"))]
+    {
         println!("cargo:rustc-link-lib=dylib=nng");
     }
+
 
     // https://rust-lang-nursery.github.io/rust-bindgen
     // https://docs.rs/bindgen
@@ -52,40 +56,18 @@ fn main() {
         .expect("Couldn't write bindings");
 }
 
-fn cmake() -> PathBuf {
-    // Compile-time features
-    let generator = if cfg!(feature = "cmake-ninja") {
-        "Ninja"
-    } else if cfg!(feature = "cmake-vs2017") {
-        "Visual Studio 15 2017"
-    } else if cfg!(feature = "cmake-vs2017-win64") {
-        "Visual Studio 15 2017 Win64"
-    } else {
-        // Default generators
-        if cfg!(target_family = "unix") {
-            "Unix Makefiles"
-        } else {
-            "Ninja"
-        }
-    };
-    let stats = if cfg!(feature = "nng-stats") {
-        "ON"
-    } else {
-        "OFF"
-    };
-    let tls = if cfg!(feature = "nng-tls") {
-        "ON"
-    } else {
-        "OFF"
-    };
+fn generator() -> &'static str {
+    let ninja = cfg!(feature = "cmake-ninja");
+    let vs2017 = cfg!(feature = "cmake-vs2017");
+    let vs2017_win64 = cfg!(feature = "cmake-vs2017-win64");
+    let unix = cfg!(target_family = "unix");
 
-    // Run cmake to build nng
-    Config::new("nng")
-        .generator(generator)
-        .define("CMAKE_BUILD_TYPE", "Release")
-        .define("NNG_TESTS", "OFF")
-        .define("NNG_TOOLS", "OFF")
-        .define("NNG_ENABLE_STATS", stats)
-        .define("NNG_ENABLE_TLS", tls)
-        .build()
+    match (ninja, vs2017, vs2017_win64, unix) {
+        (true, false, false, _) => "Ninja",
+        (false, true, false, _) => "Visual Studio 15 2017",
+        (false, false, true, _) => "Visual Studio 15 2017 Win64",
+        (false, false, false, true) => "Unix Makefiles",
+        (false, false, false, false) => "Ninja",
+        _ => panic!("More than one CMake generator selected"),
+    }
 }

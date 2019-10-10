00c8c63f75349a1e0cd5bf74ccb073d99d2deb76
jeikabu <jeikabu@users.noreply.github.com>
2019-10-10 16:10:06 +0300

    Retire "jake-ruyi" Appveyor and enable VS2019 builds
    - Powershell treats writes to stderr as exceptions. Anything about crates.io goes to stderr... need to just ignore errors
    -
    https://help.appveyor.com/discussions/problems/5413-calling-external-executable-causes-nativecommanderror-despite-no-apparent-error

--- build.ps1
+++ build.ps1
@@ -1,9 +1,18 @@
-# Non-terminating errors fail the script immediately
-$ErrorActionPreference = "Stop"
+$ErrorActionPreference = "SilentlyContinue"
 
+# Need to redirect stderr because powershell treats as exceptions which fail appveyor
 if ($env:platform -eq "x86") {
-    cargo test --features cmake-vs2017
+    # For 32-bit builds explicitly set the feature to use the correct Visual Studio generator
+    if ($env:vs_ver -eq "2017") {
+        cargo test --features cmake-vs2017
+    } elseif ($env:vs_ver -eq "2019") {
+        cargo test --features cmake-vs2019
+    }
+    else {
+        cargo test
+    }
 } else {
+    # For 64-bit builds auto-detect correct generator but rebuild bindings from source
     cargo test --features build-bindgen
     cargo build --features source-update-bindings
 }

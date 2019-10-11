a6b8f735a212b03bc9a4a1fca0804338336c8308
jeikabu <jeikabu@users.noreply.github.com>
2019-10-11 14:57:44 +0300

    Set up CI with Azure Pipelines
    - Working CI/CD via Azure Pipelines

---
+++ scripts/install_rust.ps1
@@ -0,0 +1,9 @@
+if ($IsWindows) {
+    Invoke-WebRequest https://win.rustup.rs/ -OutFile rustup-init.exe
+    ./rustup-init.exe -yv --default-toolchain stable --default-host x86_64-pc-windows-msvc
+} elseif ($IsMacOS) {
+    Invoke-WebRequest https://sh.rustup.rs -OutFile rustup-init.sh
+    bash rustup-init.sh -y -v --default-toolchain stable
+} else {
+
+}
